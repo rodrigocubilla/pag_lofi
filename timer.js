3f4852203591b58d9c90f385086b46fbccfd94d6
rodrigocubilla <81375832+rodrigocubilla@users.noreply.github.com>
2022-03-24 22:17:55 -0300

    Add files via upload

---
+++ timer.js
@@ -0,0 +1,62 @@
+const el = document.querySelector(".clock");
+const bell = document.querySelector("audio");
+const clock = document.querySelector(".reloj");
+
+const mindiv = document.querySelector(".mins");
+const secdiv = document.querySelector(".secs");
+
+const startBtn = document.querySelector(".start");
+localStorage.setItem("btn", "focus");
+
+let initial, totalsecs, perc, paused, mins, seconds;
+
+startBtn.addEventListener("click", () => {
+  let btn = localStorage.getItem("btn");
+
+  if (btn === "focus") {
+    mins = +localStorage.getItem("focusTime") || 1;
+  } else {
+    mins = +localStorage.getItem("breakTime") || 1;
+  }
+
+  seconds = mins * 60;
+  totalsecs = mins * 60;
+  setTimeout(decremenT(), 60);
+  startBtn.style.transform = "scale(0)";
+  paused = false;
+});
+
+function decremenT() {
+  mindiv.textContent = Math.floor(seconds / 60);
+  secdiv.textContent = seconds % 60 > 9 ? seconds % 60 : `0${seconds % 60}`;
+  if (circle.classList.contains("danger")) {
+    circle.classList.remove("danger");
+    // clock.play();
+  }
+
+  if (seconds > 0) {
+    perc = Math.ceil(((totalsecs - seconds) / totalsecs) * 100);
+    setProgress(perc);
+    seconds--;
+    initial = window.setTimeout("decremenT()", 1000);
+    if (seconds < 10) {
+      circle.classList.add("danger");
+    }
+  } else {
+    mins = 00;
+    seconds = 00;
+    //bell.play();
+    let btn = localStorage.getItem("btn");
+
+    if (btn === "focus") {
+      startBtn.textContent = "Descanso";
+      startBtn.classList.add("break");
+      localStorage.setItem("btn", "break");
+    } else {
+      startBtn.classList.remove("break");
+      startBtn.textContent = "Concentrate";
+      localStorage.setItem("btn", "focus");
+    }
+    startBtn.style.transform = "scale(1)";
+  }
+}
